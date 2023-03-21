68928f403e0019ea66fc189dabf097ae40c9811d
Rizwan Ahmad <rizwan.ahmed@devsspace.com>
2023-03-21 22:36:19 +0500

    email work completed

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,10 +1,19 @@
 <h2>Hey !</h2> <br><br>
-
-You received an email from : {{  $data['name'] }} <br><br>
-
+Following is my Information Please provide assistance
+in booking. 
+<br><br>
 User details: <br><br>
 
 Name:  {{ $data['name'] }}<br>
 Email:  {{ $data['email']}}<br>
+Phone:  {{ $data['phone']}}<br>
+Country:  {{ $data['country']}}<br>
+British Citizenship:  {{ $data['british_citizen']}}<br>
+Taxi:  {{ $data['taxi']}}<br>
+Hotel:  {{ $data['hotel']}}<br>
+Hotel:  {{ $data['ticket']}}<br>
+Umrah Package:  {{ $data['umrah_package']}}<br>
+Pickup Location:  {{ $data['pickup']}}<br>
+Drop Off Location:  {{ $data['destination']}}<br>
 
 Thanks
